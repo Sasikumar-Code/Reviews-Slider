cd097e605bf961e150ecf66725c82178ebc7e200
Sasikumar-Code <sasiias2024@gmail.com>
2024-01-09 00:45:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Auto Slinding Review React App....
+
+![Screenshot 2022-12-18 202755](https://user-images.githubusercontent.com/107635975/208305395-a5ed2bb6-1700-4330-9ab5-70a1fe152520.png)
+
---
+++ src/data.js
@@ -0,0 +1,37 @@
+const people = [
+  {
+    id: 1,
+    image:
+      "https://staticg.sportskeeda.com/editor/2022/08/53e15-16596004347246.png",
+    name: "Naruto Uzumaki",
+    title: "7 hokage",
+    quote: "Hard work is worthless for those that don't believe in themselves.",
+  },
+  {
+    id: 2,
+    image:
+      "https://i.pinimg.com/originals/d3/e6/4b/d3e64bd61d20fd7500e8e8464978c1ec.jpg",
+    name: "Minato Namikaze",
+    title: "Naruto's Father",
+    quote:
+      "If a man hasn't discovered something that he will die for, he isn't fit to live.",
+  },
+  {
+    id: 3,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLZftC52Cz_0WNCGdTI5MR6cPrOxi3Lhk0zg&usqp=CAU",
+    name: "Kakashi Hatake",
+    title: "Naruto Teacher",
+    quote: "Those Without Many Abilities Tend To Complain More.",
+  },
+  {
+    id: 4,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSY-_XtFZ3P0haqLHYGDpWktqG5Q-XQhyfuzNLtSIRHwPkecuTq0dagbpy6ozmdKNqaW2Y&usqp=CAU",
+    name: "Sasuke Uchiha",
+    title: "Naruto Best Friend",
+    quote: "It's Not The Future I Dream Of Anymore. Only The Past.",
+  },
+];
+
+export default people;
